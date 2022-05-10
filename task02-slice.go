2491d073fb1babb9ddeaa74b2036aa7d953bf186
GremlinBS <borodxxx@gmail.com>
2022-05-11 02:12:19 +0300

    1st try

--- task02-slice.go
+++ task02-slice.go
@@ -1,6 +1,8 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	//Place your code here
-	return
+	for i := 0; i < len(input); i++ {
+		result = append(result, input[i])
+	}
+	return result
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,16 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+	keys := make([]string, 0, len(input))
+	for k := range input {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for k := range keys {
+		result = append(result, input[k])
+	}
+	return result
 }
